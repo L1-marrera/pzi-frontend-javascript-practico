a0d5d80458d7aecc5f919c78fd8b00efe61e4a5a
Luis De La Rosa <luismarrera10@gmail.com>
2023-10-09 15:06:22 -0400

    Agregando mobile menu y arreglando media queries

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamMenuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+hamMenuIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(event) {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(event) {
+    mobileMenu.classList.toggle('inactive');
 }
